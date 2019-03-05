2c7ca5bc7959ac12458435c10d48d9f7440e97f4
Daniel Field <dannyf506@googlemail.com>
2019-03-05 22:16:38 +0000

    Added contact form and handled the POST method on the back end by writing the users imput txt to a form.txt file

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
 
@@ -29,8 +29,14 @@ def about_member(member_name):
                 
     return render_template("member.html", member=member)            
                 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
+    if request.method =="POST":
+        f = open("form.txt", "w")
+        text = request.form["name"] + "\n" + request.form["email"] + "\n" + request.form["phone"] + "\n" + request.form["message"] + "\n"
+        f.writelines(text)
+        f.close()
+        
     return render_template("contact.html", page_title="Contact")
 
 
